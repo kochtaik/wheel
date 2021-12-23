b31b35c93a0477cbcbb14ec1b6ee3ef314c91f2e
kanstantsin lazouski <kochtaik@gmail.com>
2021-12-23 23:36:46 +0300

    add output, change rotation range

--- src/TestGame.ts
+++ src/TestGame.ts
@@ -10,6 +10,8 @@ export class TestGame {
   private button: PIXI.Sprite;
   private sectors: Array<PIXI.Container>;
   private winningValue: Number;
+  private messageContainer: PIXI.Text;
+
   public initialize(): void {
     const appOptions: any = {
       width: 800,
@@ -27,8 +29,6 @@ export class TestGame {
     this.addFace();
     this.addInteractions();
     this.addListeners();
-
-    this.app.ticker.add(() => {});
   }
 
   protected loadResources(): void {
@@ -60,38 +60,51 @@ export class TestGame {
   }
 
   private addListeners() {
-    const input = document.querySelector("#winning-value") as HTMLInputElement;
+    if (!this.winningValue) {
+      const randomValue = Math.round(getRandomInRange(5, 25) / 5) * 5;
+      this.setWinningValue(randomValue);
+    }
 
-    input.addEventListener("input", (e) => {
+    const input = document.querySelector("#winning-value") as HTMLInputElement;
+    const form = document.querySelector("#winning-form");
+    const errorMsg = document.querySelector(".error");
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
       const value = parseInt(input.value);
-      if (value <= 0 || value > 25 || value % 5 !== 0) return;
+      if (value <= 0 || value > 25 || value % 5 !== 0) {
+        return errorMsg.classList.add("show-error");
+      }
+      errorMsg.classList.remove("show-error");
       this.setWinningValue(value);
     });
   }
 
   private setWinningValue(value: number) {
+    const outputValue = document.querySelector("#output");
+    outputValue.textContent = String(value);
     this.winningValue = value;
   }
 
   addFace() {
-    const radius = 170;
+    const radius = 180;
     this.sectors = [];
     const container = new PIXI.Container();
-    container.pivot.set(350, 10);
+    container.pivot.set(400, 20);
 
     for (let i = 0; i < 6; i += 1) {
       const content = String((i + 1) * 5);
       const number = new PIXI.Text(content);
       number.name = content;
       number.position.set(
-        radius / 0.9 + (Math.cos((i / 6) * Math.PI * 2) * radius) / 1.5,
+        radius / 1 + (Math.cos((i / 6) * Math.PI * 2) * radius) / 1.5,
         radius / 0.9 + (Math.sin((i / 6) * Math.PI * 2) * radius) / 1.5
       );
-      number.angle = 90;
+      number.angle = 90 + i * 60;
       this.sectors.push(number);
       container.addChild(number);
     }
-    container.angle = -90;
+    container.angle = -95;
     this.wheel.addChild(container);
   }
 
@@ -100,16 +113,16 @@ export class TestGame {
     this.button.buttonMode = true;
 
     this.button.on("pointerdown", () => {
+      this.resetState();
       this.button.scale.x = 0.9;
       this.button.scale.y = 0.9;
       const duration = 2;
       gsap.to(this.wheel, {
         duration: duration,
-        rotation: getRandomInRange(6.283, 18.849),
+        rotation: getRandomInRange(12.5664, 18.849),
       });
 
       setTimeout(this.alignWheel.bind(this), duration * 1000);
-      // this.checkIfWon();
     });
 
     this.button.on("pointerup", () => {
@@ -128,17 +141,50 @@ export class TestGame {
       angle: nearest,
     });
 
-    // this.checkIfWon();
+    this.checkIfWon();
   }
 
-  checkIfWon() {}
+  checkIfWon() {
+    const tongueBounds = this.wheelTongue.getBounds();
+    const sorted = [...this.sectors].sort((a, b) => {
+      const aDistance = Math.abs(tongueBounds.y - a.getBounds().y);
+      const bDistance = Math.abs(tongueBounds.y - b.getBounds().y);
+      if (aDistance > bDistance) return 1;
+      if (aDistance < bDistance) return -1;
+      return 0;
+    });
 
-  rotate(delta: number) {
-    const rotation = this.easeOutCubic(delta);
-    this.wheel.rotation += rotation;
+    const nearest = sorted[0];
+    // @ts-ignore
+    const droppedValue = parseInt(nearest.text);
+    if (droppedValue === this.winningValue) {
+      this.showMessage("you've won!");
+      console.log("you've won!");
+    } else {
+      this.showMessage("you've lost!");
+      console.log("you've lost!");
+    }
+    console.log(droppedValue, this.winningValue);
   }
 
-  easeOutCubic(x: number): number {
-    return 1 - Math.pow(1 - x, 3);
+  showMessage(message: string) {
+    const style = new PIXI.TextStyle({
+      align: "center",
+      fontFamily: "Comic Sans MS",
+      fontSize: 28,
+      fontWeight: "bold",
+      lineJoin: "round",
+      stroke: "white",
+      wordWrap: true,
+    });
+
+    this.messageContainer = new PIXI.Text(message, style);
+    console.log(this.container.position);
+    this.messageContainer.x = 350;
+    this.container.addChild(this.messageContainer);
+  }
+
+  resetState() {
+    this.messageContainer?.destroy();
   }
 }
